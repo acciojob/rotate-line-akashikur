48167b6228ef7fa5067fac5b0a98381d77c6a27d
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-02 19:34:42 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-var line = document.getElementById("line");
-var lineRect = line.getBoundingClientRect();
-var lineCenterX = lineRect.left + lineRect.width / 2;
-var lineCenterY = lineRect.top + lineRect.height / 2;
-
-line.style.transformOrigin = lineCenterX + "px " + lineCenterY + "px";
-line.style.transform = "rotate(45deg)";
